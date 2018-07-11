41be06541c1165c08a52fd68bc09131c867fe878
jbaudry@gmail.com <jbaudry@gmail.com>
2018-07-11 10:52:36 +0200

    First deployment of Flask boilerplate

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 # wsgi.py
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,4 +13,4 @@ def products():
     { 'id': 1, 'name': 'Skello' },
     { 'id': 2, 'name': 'Socialive.tv' }
 ]
-    return jsonify(the_product)        )
+    return jsonify(the_products)
