307956a21e1fee2825059f4a77d7e8fc9e24d661
jbaudry@gmail.com <jbaudry@gmail.com>
2018-07-11 14:33:16 +0200

    lalala

---
+++ tests/test_view.py
@@ -0,0 +1,40 @@
+# tests/test_views.py
+from flask_testing import TestCase
+from wsgi import app
+
+class TestViews(TestCase):
+    def create_app(self):
+        app.config['TESTING'] = True
+        return app
+
+    def test_products_json(self):
+        response = self.client.get("/api/v1/products")
+        products = response.json
+        self.assertIsInstance(products, list)
+        self.assertGreater(len(products), 3)
+
+    def test_get_valid_products(self):
+        response = self.client.get("/api/v1/products/3")
+        #print(response.json)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_invalid_products(self):
+        response = self.client.get("/api/v1/products/244")
+        #print(response.status_code)
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_invalid_products(self):
+        response = self.client.delete("/api/v1/products/244")
+        #print(response.status_code)
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_valid_products(self):
+        response = self.client.delete("/api/v1/products/3")
+        #print(response.status_code)
+        self.assertEqual(response.status_code, 204)
+
+    def test_create_valid_products(self):
+        response = self.client.post("/api/v1/products")
+        #print(response.status_code)
+        self.assertEqual(response.status_code, 201)
+
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 # wsgi.py
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
@@ -8,9 +8,38 @@ def hello():
     return "Hello World!"
 
 @app.route('/api/v1/products')
-def products():
+def get_products():
     the_products = [
     { 'id': 1, 'name': 'Skello' },
-    { 'id': 2, 'name': 'Socialive.tv' }
+    { 'id': 2, 'name': 'Socialive.tv' },
+    { 'id': 3, 'name': 'Nothing' },
+    { 'id': 4, 'name': 'Here.tv' }
 ]
     return jsonify(the_products)
+
+@app.route('/api/v1/products/<id_product>')
+def get_product(id_product):
+    the_products = [
+    { 'id': 1, 'name': 'Skello' },
+    { 'id': 2, 'name': 'Socialive.tv' },
+    { 'id': 3, 'name': 'Nothing' },
+    { 'id': 4, 'name': 'Here.tv' }
+]
+    for elem in the_products:
+        if elem['id'] == int(id_product):
+            return jsonify(elem)
+    abort(404)
+
+@app.route('/api/v1/products/<id_product>', methods=['DELETE'])
+def delete_product(id_product):
+    the_products = [
+    { 'id': 1, 'name': 'Skello' },
+    { 'id': 2, 'name': 'Socialive.tv' },
+    { 'id': 3, 'name': 'Nothing' },
+    { 'id': 4, 'name': 'Here.tv' }
+]
+    for elem in the_products:
+        if elem['id'] == int(id_product):
+            the_products.remove(elem)
+            return ('', 204)
+    abort(404)
